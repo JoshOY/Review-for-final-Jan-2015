2691d304fc6bb04aed19915adff955bd9cda7dba
Josh Ouyang <JoshOY789@gmail.com>
2015-01-07 01:26:44 +0800

    Update.

--- 现代数值计算/04-几个常用的积分公式/midpoint_rule.R
+++ 现代数值计算/04-几个常用的积分公式/midpoint_rule.R
@@ -2,7 +2,7 @@ midpoint_rule <- function(func_expression, a, b, ksi = NA) {
 	if (is.na(ksi)) {
 		ksi <- (a + b) / 2
 	}
-	func <- deriv(func_expression)
+	func <- deriv3(func_expression)
 	ddfunc <- attr(func(ksi), 'hessian')
 	rtn <- ((b - a) ^ 3) * ddfunc / 24
 	rtn
